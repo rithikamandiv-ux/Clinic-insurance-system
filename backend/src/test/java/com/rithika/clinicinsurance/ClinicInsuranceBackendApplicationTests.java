e2d793340b7e36c438393292d0ecf64e20153818
Rithika-077 <rithikamandiv@gmail.com>
2026-09-12 00:53:10 +0530

    chore: separate development and test configuration

--- backend/src/test/java/com/rithika/clinicinsurance/ClinicInsuranceBackendApplicationTests.java
+++ backend/src/test/java/com/rithika/clinicinsurance/ClinicInsuranceBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.rithika.clinicinsurance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClinicInsuranceBackendApplicationTests {
 
 	@Test
